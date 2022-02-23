5fa84e72ac929da0ec5d4bdaf1a7ab5c6d205005
FelipeSakaguti <felipesakaguti@hotmail.com>
2022-02-23 20:06:52 -0300

    Created Details Page

---
+++ src/screens/CarDetails/index.tsx
@@ -0,0 +1,92 @@
+import React from 'react';
+
+import { BackButton } from '../../components/BackButton';
+import { ImageSlider } from '../../components/ImageSlider';
+import { Acessory } from '../../components/Acessory';
+import { Button } from '../../components/Button';
+
+import speedSvg from '../../assets/speed.svg'
+import AccelerationSvg from '../../assets/acceleration.svg'
+import ForceSvg from '../../assets/force.svg'
+import GasolineSvg from '../../assets/gasoline.svg'
+import ExchangeSvg from '../../assets/exchange.svg'
+import PeopleSvg from '../../assets/people.svg'
+
+import {
+    About,
+    Acessories,
+    Brand,
+    CarImages,
+    Container,
+    Content,
+    Description,
+    Details,
+    Footer,
+    Header,
+    Name,
+    Period,
+    Price,
+    Rent
+} from './styles';
+
+export function CarDetails() {
+    return (
+        <Container>
+            <Header>
+                <BackButton
+                    onPress={() => { }}
+                />
+            </Header>
+
+            <CarImages>
+                <ImageSlider
+                    imagesUrl={['https://production.autoforce.com/uploads/version/profile_image/3188/model_main_comprar-tiptronic_87272c1ff1.png']}
+                />
+            </CarImages>
+
+            <Content>
+                <Details>
+                    <Description>
+                        <Brand>Lamborghini</Brand>
+                        <Name>Huracan</Name>
+                    </Description>
+
+                    <Rent>
+                        <Period>Ao dia</Period>
+                        <Price>R$ 580</Price>
+                    </Rent>
+                </Details>
+
+                <Acessories>
+                    <Acessory name="380km/h" icon={speedSvg} />
+                    <Acessory name="3.2s" icon={AccelerationSvg} />
+                    <Acessory name="800 HP" icon={ForceSvg} />
+                    <Acessory name="Gasolina" icon={GasolineSvg} />
+                    <Acessory name="Auto" icon={ExchangeSvg} />
+                    <Acessory name="2 pessoas" icon={PeopleSvg} />
+                </Acessories>
+
+                <About>
+                    Este é automóvel desportivo. Surgiu do lendário touro de lide indultado
+                    na praça Real Maestranza de Sevilla. É um belíssimo carro para quem gosta de acelerar.
+                </About>
+
+                <About>
+                    Este é automóvel desportivo. Surgiu do lendário touro de lide indultado
+                    na praça Real Maestranza de Sevilla. É um belíssimo carro para quem gosta de acelerar.
+                </About>
+
+                <About>
+                    Este é automóvel desportivo. Surgiu do lendário touro de lide indultado
+                    na praça Real Maestranza de Sevilla. É um belíssimo carro para quem gosta de acelerar.
+                </About>
+
+            </Content>
+
+            <Footer>
+                <Button title="Confirmar" />
+            </Footer>
+
+        </Container>
+    )
+}
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,14 +4,27 @@ import { RFValue } from 'react-native-responsive-fontsize'
 
 import Logo from '../../assets/logo.svg';
 
+import { Car } from '../../components/Car';
+
 import {
     Container,
     Header,
     HeaderContent,
-    TotalCars
+    TotalCars,
+    CarList
 } from './styles';
 
 export function Home() {
+    const carData = {
+        brand: 'audi',
+        name: 'RS 5 Coupé',
+        rent: {
+            period: 'ao dia',
+            price: 120,
+        },
+        thumbnail: 'https://production.autoforce.com/uploads/version/profile_image/3188/model_main_comprar-tiptronic_87272c1ff1.png',
+    }
+
     return (
         <Container>
             <StatusBar
@@ -30,6 +43,13 @@ export function Home() {
                     </TotalCars>
                 </HeaderContent>
             </Header>
+
+            <CarList
+                data={[1, 2, 3, 4, 5, 6, 7, 8, 9]}
+                keyExtractor={item => String(item)}
+                renderItem={({ item }) => <Car data={carData} />}
+            />
+
         </Container>
     )
 }
